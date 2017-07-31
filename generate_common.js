156a82cf82e19ca8ed693e13313bd1aba04f906a
Niles Rogoff <me@niles.xyz>
2017-07-31 12:15:51 -0700

    Fix for select one integer

--- generate_common.js
+++ generate_common.js
@@ -49,7 +49,7 @@ window.display_update_result = function display_update_result(result, this_resul
 	if (!result) result = "";
 	if (this_result !== null && this_result !== undefined && (typeof(this_result) != "string" || this_result.trim().length > 0)) {
 		if (field == "text") {
-			while (this_result.indexOf("{{data.") > 0 && _data_wrapper) {
+			while (typeof(this_result) == "string" && this_result.indexOf("{{data.") > 0 && _data_wrapper) {
 				var idx = this_result.indexOf("{{data.");
 				var beginning = this_result.substr(0, idx);
 				var rest = this_result.substr(idx + "{{data.".length);
